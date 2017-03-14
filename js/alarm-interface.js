0c1fe481735ed24042196cde88c50651edcad373
Kory Skarbek and Marc Larkin <kory+marc@>
2017-03-14 16:40:01 -0700

    alarm clock

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,8 +1,46 @@
-var AlarmClock = require('./../js/alarm.js').alarmClockModule;
-$(document).ready(function() {
-  $('#clock').text(moment().format('LTS'));
-  $('form').submit(function() {
-    event.preventDefault()
+// var AlarmClock = require('./../js/alarm.js').alarmClockModule;
+//
+function currentTime() {
+  $('#clock').html(moment().format('LT'));
+}
 
-  })
+// var alarmTime = $('#alarm-time').val();
+//
+// function checkAlarm(alarmTime){
+//   if (alarmTime === currentTime()) {
+//       #('body').addClass("alarm-off")
+//   }
+//
+// }
+
+
+
+// function alarm(alarmActive) {
+//   if (alarmActive === true) {
+//     $("body").toggleClass("alarm-off");
+//   }
+// }
+
+
+$(document).ready(function() {
+  setInterval(currentTime, 1000);
+  $('.alarm-form').submit(function() {
+    event.preventDefault();
+    var alarmTime = $('#alarm-time').val();
+    $('#alarm-set').text(alarmTime)
+setInterval(function checkAlarm(alarmTime){
+  if (alarmTime === currentTime()) {
+      $('#body').addClass("alarm-off");
+  }
+}, 1000);
+
+
+
+  });
+  // setInterval(function() {
+  //   if(moment().format("ss") % 2 === 0)
+  //   {
+  //     $("body").toggleClass("alarm-off");
+  //   }
+  // }, 1000)
 });
--- js/alarm.js
+++ js/alarm.js
@@ -1,15 +1,19 @@
-function AlarmClock(){
-  this.clocktime = moment().format('LTS')
-  this.alarmtime = user.Input
-}
-
-AlarmClock.prototype.checkTime = function () {
-  if (this. clocktime === this.alarmtime)
-  {
-    // alarm = ring;
-  }
-}
-
-setinterval(checkTime(), 1000)
-
-exports.alarmClockModule = AlarmClock;
+// var clockTime = moment().format("H:mm");
+// var alarmActive = false;
+//
+// function AlarmClock(alarmTime){
+//   this.alarmTime = alarmTime;
+// }
+//
+// AlarmClock.prototype.checkTime = setInterval(function () {
+//   if (moment().format("H:mm") === this.alarmTime)
+//   {
+//     alarmActive = true;
+//   } else {
+//     alarmActive = false;
+//   }
+//   console.log(moment().format("H:mm"));
+//   console.log(this.alarmTime);
+// }, 1000);
+//
+// exports.alarmClockModule = AlarmClock;
